d33d4bb635cf06eb00832ed8093c7d46e48a7fa7
Nikita <barash9500@mail.ru>
2019-02-19 12:45:39 +0300

    add resident functional

--- database/restate.sql
+++ database/restate.sql
@@ -1,56 +1,49 @@
--- MySQL dump 10.16  Distrib 10.1.37-MariaDB, for Win32 (AMD64)
+-- phpMyAdmin SQL Dump
+-- version 4.8.4
+-- https://www.phpmyadmin.net/
 --
--- Host: localhost    Database: restate
--- ------------------------------------------------------
--- Server version	10.1.37-MariaDB
+-- Хост: 127.0.0.1
+-- Время создания: Фев 19 2019 г., 09:44
+-- Версия сервера: 10.1.37-MariaDB
+-- Версия PHP: 7.1.26
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
--- Table structure for table `filter`
+-- База данных: `restate`
 --
 
-DROP TABLE IF EXISTS `filter`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `filter`
+--
+
 CREATE TABLE `filter` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `type` varchar(100) NOT NULL COMMENT 'тип поля фильтра',
+  `id` int(11) NOT NULL,
+  `type` varchar(100) CHARACTER SET utf8 NOT NULL COMMENT 'тип поля фильтра',
   `count` int(11) NOT NULL COMMENT 'количество нажатий',
-  `value` varchar(50) DEFAULT NULL COMMENT 'значение поля',
-  `date` date NOT NULL COMMENT 'дата',
-  `total_flat` int(11) NOT NULL COMMENT 'всего кв',
-  PRIMARY KEY (`id`)
+  `value` varchar(50) CHARACTER SET utf8 DEFAULT NULL COMMENT 'значение поля',
+  `date` date NOT NULL COMMENT 'дата'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
+
+-- --------------------------------------------------------
 
 --
--- Dumping data for table `filter`
+-- Структура таблицы `flat`
 --
 
-LOCK TABLES `filter` WRITE;
-/*!40000 ALTER TABLE `filter` DISABLE KEYS */;
-/*!40000 ALTER TABLE `filter` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `flat`
---
-
-DROP TABLE IF EXISTS `flat`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `flat` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL,
   `res_id` int(11) NOT NULL COMMENT 'id ЖК',
   `house` tinyint(4) NOT NULL COMMENT 'номер корпуса',
   `floor` tinyint(4) NOT NULL COMMENT 'этаж',
@@ -59,220 +52,239 @@ CREATE TABLE `flat` (
   `size` tinyint(4) NOT NULL COMMENT 'кол-во комнат',
   `square` float NOT NULL COMMENT 'площадь',
   `price` int(11) NOT NULL COMMENT 'цена',
-  `state` tinyint(4) DEFAULT NULL,
-  PRIMARY KEY (`id`)
+  `state` tinyint(4) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
+
+-- --------------------------------------------------------
 
 --
--- Dumping data for table `flat`
+-- Структура таблицы `housing`
 --
 
-LOCK TABLES `flat` WRITE;
-/*!40000 ALTER TABLE `flat` DISABLE KEYS */;
-/*!40000 ALTER TABLE `flat` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `housing`
---
-
-DROP TABLE IF EXISTS `housing`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `housing` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL,
   `res_id` int(11) NOT NULL COMMENT 'id ЖК',
   `house` tinyint(4) NOT NULL COMMENT 'номер корпуса',
   `floor` tinyint(4) NOT NULL COMMENT 'этажей',
   `section` tinyint(4) NOT NULL COMMENT 'секций',
   `total_flat` int(11) NOT NULL COMMENT 'всего кв',
-  `year` year(4) NOT NULL COMMENT 'год сдачи',
-  PRIMARY KEY (`id`)
+  `year` year(4) NOT NULL COMMENT 'год сдачи'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
+
+-- --------------------------------------------------------
 
 --
--- Dumping data for table `housing`
+-- Структура таблицы `ipoteka`
 --
 
-LOCK TABLES `housing` WRITE;
-/*!40000 ALTER TABLE `housing` DISABLE KEYS */;
-/*!40000 ALTER TABLE `housing` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `ipoteka`
---
-
-DROP TABLE IF EXISTS `ipoteka`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `ipoteka` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(50) NOT NULL COMMENT 'название банка',
-  `procent` float NOT NULL COMMENT 'ставка',
+  `id` int(11) NOT NULL,
+  `name` varchar(50) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL COMMENT 'название банка',
+  `percent` float NOT NULL COMMENT 'ставка',
   `year` tinyint(4) NOT NULL COMMENT 'до х лет',
-  `money` int(11) NOT NULL COMMENT 'до х рублей ',
-  PRIMARY KEY (`id`)
+  `money` int(11) NOT NULL COMMENT 'до х рублей '
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
+
+-- --------------------------------------------------------
 
 --
--- Dumping data for table `ipoteka`
+-- Структура таблицы `mapping`
 --
 
-LOCK TABLES `ipoteka` WRITE;
-/*!40000 ALTER TABLE `ipoteka` DISABLE KEYS */;
-/*!40000 ALTER TABLE `ipoteka` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `mapping`
---
-
-DROP TABLE IF EXISTS `mapping`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `mapping` (
   `res_id` int(11) NOT NULL,
   `x_pos` float NOT NULL,
-  `y_pos` float NOT NULL,
-  PRIMARY KEY (`res_id`)
+  `y_pos` float NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
+
+-- --------------------------------------------------------
 
 --
--- Dumping data for table `mapping`
+-- Структура таблицы `news`
 --
 
-LOCK TABLES `mapping` WRITE;
-/*!40000 ALTER TABLE `mapping` DISABLE KEYS */;
-/*!40000 ALTER TABLE `mapping` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `news`
---
-
-DROP TABLE IF EXISTS `news`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `news` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL,
   `res_id` int(11) DEFAULT NULL COMMENT 'id ЖК',
-  `tittle` varchar(200) NOT NULL COMMENT 'Заголовок',
-  `description` text NOT NULL COMMENT 'Описание',
-  PRIMARY KEY (`id`)
+  `tittle` varchar(200) CHARACTER SET utf8 NOT NULL COMMENT 'Заголовок',
+  `description` text CHARACTER SET utf8 NOT NULL COMMENT 'Описание',
+  `date` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
+
+-- --------------------------------------------------------
 
 --
--- Dumping data for table `news`
+-- Структура таблицы `orders`
 --
 
-LOCK TABLES `news` WRITE;
-/*!40000 ALTER TABLE `news` DISABLE KEYS */;
-/*!40000 ALTER TABLE `news` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `order`
---
-
-DROP TABLE IF EXISTS `order`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `order` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `orders` (
+  `id` int(11) NOT NULL,
   `phone` int(11) NOT NULL COMMENT 'телефон',
   `res_id` int(11) DEFAULT NULL COMMENT 'id ЖК',
   `flat_id` int(11) DEFAULT NULL COMMENT 'id кв',
   `name` varchar(50) DEFAULT NULL COMMENT 'имя',
   `ask` varchar(200) DEFAULT NULL COMMENT 'вопрос',
-  `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'дата',
-  `state` tinyint(4) NOT NULL COMMENT 'состояние',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
+  `date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'дата',
+  `state` tinyint(4) NOT NULL COMMENT 'состояние'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
 
 --
--- Dumping data for table `order`
+-- Структура таблицы `resident`
 --
 
-LOCK TABLES `order` WRITE;
-/*!40000 ALTER TABLE `order` DISABLE KEYS */;
-/*!40000 ALTER TABLE `order` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `resident`
---
-
-DROP TABLE IF EXISTS `resident`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `resident` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(50) NOT NULL COMMENT 'название жк',
-  `title` varchar(200) DEFAULT NULL COMMENT 'заголовок',
-  `description` text COMMENT 'описание',
-  `title1` varchar(100) DEFAULT NULL COMMENT 'для мелких плиток',
-  `title2` varchar(100) DEFAULT NULL COMMENT 'для мелких плиток',
-  `title3` varchar(100) DEFAULT NULL COMMENT 'для мелких плиток',
-  `desctiption1` varchar(150) DEFAULT NULL COMMENT 'для мелких плиток',
-  `desctiption2` varchar(150) DEFAULT NULL COMMENT 'для мелких плиток',
-  `desctiption3` varchar(150) DEFAULT NULL COMMENT 'для мелких плиток',
-  `metro` varchar(50) DEFAULT NULL COMMENT 'ближайшее метро',
-  `address` varchar(200) DEFAULT NULL COMMENT 'адрес ЖК',
-  `housing` tinyint(3) unsigned NOT NULL COMMENT 'кол-во корпусов',
+  `id` int(11) NOT NULL,
+  `name` varchar(50) CHARACTER SET utf8 NOT NULL COMMENT 'название жк',
+  `tittle` varchar(200) CHARACTER SET utf8 DEFAULT NULL COMMENT 'заголовок',
+  `description` text CHARACTER SET utf8 COMMENT 'описание',
+  `tittle1` varchar(100) CHARACTER SET utf8 DEFAULT NULL COMMENT 'для мелких плиток',
+  `tittle2` varchar(100) CHARACTER SET utf8 DEFAULT NULL COMMENT 'для мелких плиток',
+  `tittle3` varchar(100) CHARACTER SET utf8 DEFAULT NULL COMMENT 'для мелких плиток',
+  `description1` varchar(150) CHARACTER SET utf8 DEFAULT NULL COMMENT 'для мелких плиток',
+  `description2` varchar(150) CHARACTER SET utf8 DEFAULT NULL COMMENT 'для мелких плиток',
+  `description3` varchar(150) CHARACTER SET utf8 DEFAULT NULL COMMENT 'для мелких плиток',
+  `metro` varchar(50) CHARACTER SET utf8 DEFAULT NULL COMMENT 'ближайшее метро',
+  `address` varchar(200) CHARACTER SET utf8 DEFAULT NULL COMMENT 'адрес ЖК',
+  `housing` tinyint(3) UNSIGNED NOT NULL COMMENT 'кол-во корпусов',
   `total_flat` int(10) NOT NULL COMMENT 'кол-во квартир',
-  PRIMARY KEY (`id`,`name`)
+  `state` tinyint(4) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `resident`
+-- Дамп данных таблицы `resident`
 --
 
-LOCK TABLES `resident` WRITE;
-/*!40000 ALTER TABLE `resident` DISABLE KEYS */;
-/*!40000 ALTER TABLE `resident` ENABLE KEYS */;
-UNLOCK TABLES;
+INSERT INTO `resident` (`id`, `name`, `tittle`, `description`, `tittle1`, `tittle2`, `tittle3`, `description1`, `description2`, `description3`, `metro`, `address`, `housing`, `total_flat`, `state`) VALUES
+(1, 'First', 'Try', 'First try add resident', NULL, NULL, NULL, NULL, NULL, NULL, 'Звездное', 'ussr', 2, 2600, 1),
+(2, 'фыв', 'фыв', 'фыв', NULL, NULL, NULL, NULL, NULL, NULL, 'фыв', 'фыв', 9, 10, 2);
+
+-- --------------------------------------------------------
 
 --
--- Table structure for table `video`
+-- Структура таблицы `video`
 --
 
-DROP TABLE IF EXISTS `video`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `video` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL,
   `res_id` int(11) NOT NULL COMMENT 'id ЖК',
-  `tittle` varchar(100) NOT NULL COMMENT 'название',
-  `link` varchar(150) NOT NULL COMMENT 'ссылка на видео',
-  `date` date NOT NULL COMMENT 'дата',
-  PRIMARY KEY (`id`)
+  `tittle` varchar(100) CHARACTER SET utf8 NOT NULL COMMENT 'название',
+  `link` varchar(150) CHARACTER SET utf8 NOT NULL COMMENT 'ссылка на видео',
+  `date` date NOT NULL COMMENT 'дата'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `video`
+-- Индексы сохранённых таблиц
 --
 
-LOCK TABLES `video` WRITE;
-/*!40000 ALTER TABLE `video` DISABLE KEYS */;
-/*!40000 ALTER TABLE `video` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+--
+-- Индексы таблицы `filter`
+--
+ALTER TABLE `filter`
+  ADD PRIMARY KEY (`id`);
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+--
+-- Индексы таблицы `flat`
+--
+ALTER TABLE `flat`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `housing`
+--
+ALTER TABLE `housing`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `ipoteka`
+--
+ALTER TABLE `ipoteka`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `mapping`
+--
+ALTER TABLE `mapping`
+  ADD PRIMARY KEY (`res_id`);
+
+--
+-- Индексы таблицы `news`
+--
+ALTER TABLE `news`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `orders`
+--
+ALTER TABLE `orders`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `resident`
+--
+ALTER TABLE `resident`
+  ADD PRIMARY KEY (`id`,`name`);
+
+--
+-- Индексы таблицы `video`
+--
+ALTER TABLE `video`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT для сохранённых таблиц
+--
+
+--
+-- AUTO_INCREMENT для таблицы `filter`
+--
+ALTER TABLE `filter`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT для таблицы `flat`
+--
+ALTER TABLE `flat`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT для таблицы `housing`
+--
+ALTER TABLE `housing`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT для таблицы `ipoteka`
+--
+ALTER TABLE `ipoteka`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT для таблицы `news`
+--
+ALTER TABLE `news`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT для таблицы `orders`
+--
+ALTER TABLE `orders`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT для таблицы `resident`
+--
+ALTER TABLE `resident`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT для таблицы `video`
+--
+ALTER TABLE `video`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2019-02-15 11:12:54
